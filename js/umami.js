aa8fc088d3ccd72f579dcc2f959667aedce1ceaf
rosam01 <86015909+rosam01@users.noreply.github.com>
2025-10-30 10:00:07 -0400

    Major site update!

---
+++ js/umami.js
@@ -0,0 +1,8 @@
+// docs/js/umami.js
+(function() {
+    var s = document.createElement('script');
+    s.defer = true;
+    s.src = "https://cloud.umami.is/script.js";
+    s.dataset.websiteId = "ae9f042f-2161-4d0d-a7e4-c9cf066426b4";
+    document.head.appendChild(s);
+})();
